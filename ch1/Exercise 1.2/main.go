e27fc41abf30ea9bf4200525abc414987418010d
Lanfeng Sun <lanfeng.sun@yahoo.ca>
2018-10-17 21:45:24 -0400

    add ex 1.2

---
+++ ch1/Exercise 1.2/main.go
@@ -0,0 +1,14 @@
+// Package main prints the index and the value of each of its arguments,
+// one per line
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for i, arg := range os.Args {
+		fmt.Printf("%d: %s\n", i, arg)
+	}
+}
